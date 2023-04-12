c222affc7cd19fbb4ec97529c83f890bc506f257
abdelaziz <abdelazizsaleh1999@gmail.com>
2023-04-12 14:51:51 +0200

    first commit

---
+++ MovieApp/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApp.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string CategoryName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
---
+++ MovieApp/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.CodeAnalysis.FlowAnalysis;
+using Microsoft.EntityFrameworkCore;
+using MovieApp.Data;
+using MovieApp.Models;
+
+namespace MovieApp.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MovieApp.Data.MovieAppContext _context;
+
+        public IndexModel(MovieApp.Data.MovieAppContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public async Task OnGetAsync()
+        {
+            if (_context.Movie != null)
+            {
+                var movies = from m in _context.Movie select m;
+                if(!string.IsNullOrEmpty(SearchString))
+                {
+                    movies = movies.Where(m => m.Title.Contains(SearchString));
+                }
+                Movie = await movies.ToListAsync();
+            }
+        }
+    }
+}
